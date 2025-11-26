1c1562efe715b7b32f420fd77d3edaad39cbc6bd
RoboticsKnight <toluwanimiomoyeni30@gmail.com>
2025-11-26 13:01:10 +0200

    First Commit

--- Hand_Gesture.py
+++ Hand_Gesture.py
@@ -1,3 +1,29 @@
-print("Hello World")
-print("E")
-print("EEE")
+#Run using python interprator 13.2
+
+#=========================================================
+#Libraries
+#=========================================================
+import csv
+import copy
+import argparse
+import itertools
+from collections import Counter
+from collections import deque
+
+import cv2 as cv
+import numpy as np
+import mediapipe as mp
+
+from utils import CvFpsCalc
+from model import KeyPointClassifier
+from model import PointHistoryClassifier
+
+
+def get_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--device", type = int, default=0)
+
+
+
+
